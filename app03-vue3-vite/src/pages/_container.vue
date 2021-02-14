3f225527641876152dea6db45276ba5b9c36f4da
seniya2 <seniya2@gmail.com>
2021-02-15 01:24:26 +0900

    feat: init

---
+++ app03-vue3-vite/src/pages/_container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    _container
+  </div>
+  <div>
+    <p>
+      <router-link to="/">Go to Home</router-link>
+    </p>
+    <p>
+      <router-link to="/chat">Go to Chat</router-link>
+    </p>
+  </div>
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Container',
+})
+</script>
+
+<style>
+
+</style>
---
+++ app03-vue3-vite/src/pages/chat.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Chat
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Chat',
+})
+</script>
+
+<style>
+
+</style>
